505d1f8196020a0e1725860a84fb54d4387de8d4
Nitish Malhotra <nitishm@microsoft.com>
2022-01-31 13:33:59 -0800

    Insert one document to collection
    Signed-off-by: Nitish Malhotra <nitishm@microsoft.com>

--- crates/mongo-wasmtime/src/lib.rs
+++ crates/mongo-wasmtime/src/lib.rs
@@ -6,22 +6,46 @@ pub use wasi_mongo::add_to_linker;
 use wasi_mongo::*;
 use std::sync::Arc;
 
-use mongodb::sync::{Client,Database};
+use mongodb::{
+    sync::{Client,Database,Collection},
+    options::CreateCollectionOptions, bson::{Bson, document::Values, oid::ObjectId}, results::InsertOneResult,
+};
+
+
+use serde_json::{
+    Value,
+    Map,
+    from_slice,
+};
+
+const MONGO_DOCUMENT_ID_KEY: &str = "_id";
 
 wit_bindgen_wasmtime::export!("wit/ephemeral/wasi-mongo.wit");
 
 #[derive(Clone)]
 pub struct WasiMongoClient {
-    address: String,
     client: Arc<Client>
 }
 
 impl wasi_mongo::WasiMongo for WasiMongoClient {
-    fn collection_exists(&mut self, db: &str, name: &str) -> Result<bool, Error>{
-        if let Ok(()) = self.collection_exists(db, name) {
-            return Ok(true)
-        } 
-        Err(Error::Error) 
+    fn create_collection(&mut self, db: &str, name: &str) -> Result<(), Error> {
+        self.mongo_create_collection(db, name)?;
+        Ok(())
+    }
+
+    fn insert_one_document(&mut self, db: &str, collection: &str, id: &str, document: wasi_mongo::DocumentParam) -> Result<String, Error> {
+        match self.mongo_insert_one_document(db, collection, id, document) {
+            Ok(res) => {
+                println!("{:?}", res);
+                return Ok(res.inserted_id.as_str().ok_or_else(|| Error::Error)?.to_string());
+            },
+            Err(_) => Err(Error::Error),
+        }
+    }
+
+    fn get_document(&mut self, db: &str, collection: &str, name: &str) -> Result<wasi_mongo::DocumentResult, Error> {
+        self.mongo_get_document(db, collection, name)?;
+        Ok(wasi_mongo::DocumentResult::new())
     }
 }
 
@@ -29,25 +53,41 @@ impl WasiMongoClient {
     pub fn new(addr: &str) -> anyhow::Result<Self> {
         let client = Arc::new(Client::with_uri_str(addr)?);
         Ok(WasiMongoClient {
-            address: addr.to_string(),
             client
         })
     }
 
-    fn collection_exists(&mut self, db: &str, _name: &str) -> anyhow::Result<()> {
-        if let Ok(database) = self.database(db) {
-            if let Ok(_doc) = database.run_command(mongodb::bson::doc! {"ping": 1}, None) {
-                return Ok(())
-            } else {
-               return Err(anyhow::anyhow!("Database {} does not exist", db)) 
-            }
-        }
-        Err(anyhow::Error::msg("database does not exist"))
-    }
-    
     fn database(&mut self, name: &str) -> anyhow::Result<Database> {
         let db = self.client.database(name);
         Ok(db)
+    }
+
+    fn mongo_create_collection(&mut self, db: &str, name: &str) -> anyhow::Result<()> {
+        let database = self.database(db)?;
+        database.create_collection(name, CreateCollectionOptions::default())?;
+        Ok(())
+    }
+
+    fn mongo_insert_one_document(&mut self, db: &str, collection: &str, id: &str, document: wasi_mongo::DocumentParam) -> anyhow::Result<InsertOneResult> {
+        let database = self.database(db)?;
+        let collection: Collection<Map<String, Value>> = database.collection(collection);
+        
+        let parsed: Value = from_slice(document)?;
+        let mut obj = parsed.as_object().unwrap().clone();
+        
+        // Add the id to the documents _id field
+        obj.insert(MONGO_DOCUMENT_ID_KEY.to_string(), Value::String(id.to_string()));
+        
+        Ok(collection.insert_one(obj, None)?)
+    }
+
+    fn mongo_get_document(&mut self, db: &str, collection: &str, _name: &str) -> anyhow::Result<wasi_mongo::DocumentResult> {
+        let database = self.database(db)?;
+        let _collection: Collection<Map<String, Value>> = database.collection(collection);
+
+        // let obj: Map<String, Value> = collection.find_one(doc! {"_id": name}, None)?.unwrap();
+
+        Ok(wasi_mongo::DocumentResult::new())
     }
 }
 
--- tests/modules/rust-mongo/src/lib.rs
+++ tests/modules/rust-mongo/src/lib.rs
@@ -3,12 +3,22 @@ use wasi_mongo::*;
 wit_bindgen_rust::import!("../../../wit/ephemeral/wasi-mongo.wit");
 wit_bindgen_rust::export!("../../test.wit");
 
+const TEST_DB_NAME: &str = "db";
+const TEST_COLLECTION_NAME: &str = "collection";
+const TEST_DOCUMENT_ID: &str = "my_id";
+
 struct Test {}
 
 impl test::Test for Test {
     fn test() -> Result<(), test::Error> {
-        let msg: &str = "To err is human to rub it in is divine";
-        wasi_mongo::collection_exists("database", "collection")?;
+
+
+        wasi_mongo::create_collection(TEST_DB_NAME, TEST_COLLECTION_NAME)?;
+        
+        let doc: &[u8] = 
+            r#"{ "name": "Nitish", "age": 31, "city": "San Diego" }"#.as_bytes();
+        wasi_mongo::insert_one_document(TEST_DB_NAME, TEST_COLLECTION_NAME, TEST_DOCUMENT_ID, doc.into())?;
+        
         Ok(())
     }
 }
